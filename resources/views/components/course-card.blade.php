89bc8ea7ef0a09ac67fe0de4836ba67a11b759dd
omonosh1999 <godsonogundare25@gmail.com>
2025-11-03 00:34:20 +0100

    nav fix

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -60,7 +60,7 @@
     </div>
 
     <!-- Content Section -->
-    <div class="p-6 flex flex-col h-64">
+    <div class="p-6 flex flex-col">
         <!-- Title -->
         <h3 class="text-3xl font-bold text-gray-900 mb-3 group-hover:text-[#006738] transition-colors duration-200">
             {{ $title }}
